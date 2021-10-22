9ea70ff8ed2af6d6d42e19647c61dd04310a8d12
Vuk Milanović <vuk.milanovic11@gmail.com>
2021-10-22 20:07:55 +0200

    Scanner_edit

--- src/kalkulator/Main.java
+++ src/kalkulator/Main.java
@@ -1,10 +1,15 @@
 package kalkulator;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		String str;
+		str = scanner.nextLine();
+		System.out.println(str);
 	}
 
 }
